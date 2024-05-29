d4e08b45de045a32a0e641d9eac68e83d917529c
PolyMath-Coder <you@example.com>
2024-05-29 12:40:06 +0100

    chore: minor refactor

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalRoute } from './shared/constants';
 import { ValidationPipe } from '@nestjs/common';
-
+import { config } from 'dotenv';
+config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({transform: true}))
   app.setGlobalPrefix(GlobalRoute.PREFIX);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
