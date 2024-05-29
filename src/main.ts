8b0dbf62a8629b9870f23c753533a096b7af0328
PolyMath-Coder <you@example.com>
2024-05-29 13:21:38 +0100

    chore: make slight fix

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({transform: true}))
   app.setGlobalPrefix(GlobalRoute.PREFIX);
-  await app.listen(process.env.PORT);
+  await app.listen(3000);
 }
 bootstrap();
